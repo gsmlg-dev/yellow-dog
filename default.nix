3371b617b12a0bd9f8239669911fa04bd8ffb15b
Jonathan Gao <gsmlg.com@gmail.com>
2024-06-04 17:29:10 +0800

    fix: Add nix support.

---
+++ default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  pkgs,
+  buildNpmPackage,
+  beamPackages,
+  nodejs,
+  ...
+}: let
+  pname = "yellow_dog";
+  version = "1.0.0";
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = ./.;
+  };
+
+  mixFodDeps = beamPackages.fetchMixDeps {
+    pname = "${pname}-mix-deps";
+    inherit src version;
+    # nix will complain and tell you the right value to replace this with
+    hash = "sha256-CIceAuuNZFlUCHywj4N3pFvQpGT8SqjfT7IftUfhz2o=";
+    mixEnv = "prod"; # default is "prod", when empty includes all dependencies, such as "dev", "test".
+    # if you have build time environment variables add them here
+    RELEASE_COOKIE = "Best_in_the_World!";
+  };
+in
+  beamPackages.mixRelease {
+    inherit pname version src mixFodDeps;
+
+    nativeBuildInputs = [
+    ];
+
+    preBuild = ''
+    '';
+
+    postBuild = ''
+    '';
+
+    meta = with lib; {
+      description = "YellowDog NameServer for GSMLG.dev";
+      mainProgram = "yellow_dog";
+    };
+  }
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      app = pkgs.callPackage ./default.nix {inherit system;};
+      img = pkgs.dockerTools.buildImage {
+        name = "ghcr.io/gsmlg-dev/yellowdogdns";
+        tag = "latest";
+        created = "now";
+        copyToRoot = pkgs.buildEnv {
+          name = "image-root";
+          paths = [
+            pkgs.busybox
+            pkgs.dockerTools.usrBinEnv
+            pkgs.dockerTools.binSh
+            pkgs.dockerTools.caCertificates
+            pkgs.dockerTools.fakeNss
+            app
+          ];
+          pathsToLink = ["/bin" "/etc" "/var"];
+        };
+
+        config = {
+          Entrypoint = ["/bin/yellow_dog"];
+          Cmd = ["start"];
+          Env = [
+            "REPLACE_OS_VARS=true"
+            "ERL_EPMD_PORT=4369"
+            "ERLCOOKIE=96myjWoLCTZRko08UdngkxQo/SwP9vfga28/B6IL"
+            "POOL_SIZE=10"
+            "RELEASE_COOKIE=3swYaASXT0ARmMHUjiDsesPesG0hh/SMQbQx6kX4+Z6+9S9YA2lS6lVNdQiX93Wv"
+          ];
+        };
+      };
+    in {
+      packages.docker = img;
+      defaultPackage = app;
+    });
+}
