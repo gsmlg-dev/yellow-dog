a1948a2eca47745926a79fa6cb24cc7cb90b8574
Jonathan Gao <gsmlg.com@gmail.com>
2024-06-04 17:35:09 +0800

    fix: Fix lock.

--- default.nix
+++ default.nix
@@ -18,7 +18,7 @@
     pname = "${pname}-mix-deps";
     inherit src version;
     # nix will complain and tell you the right value to replace this with
-    hash = "sha256-CIceAuuNZFlUCHywj4N3pFvQpGT8SqjfT7IftUfhz2o=";
+    hash = "sha256-sZgrfqjP8ytzWE8YlnXddQve7eClmFOCb4nOy1Zl8Ro=";
     mixEnv = "prod"; # default is "prod", when empty includes all dependencies, such as "dev", "test".
     # if you have build time environment variables add them here
     RELEASE_COOKIE = "Best_in_the_World!";
