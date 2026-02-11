05ee792db8ce3ed4efe45e8bbd1c0d6d18416256
Jonathan Gao <gsmlg.com@gmail.com>
2026-02-12 00:35:08 +0800

    fix(console): use distinct bolt icon for netboot sidebar section

--- apps/yellow_dog_console/lib/yellow_dog/console/components/layouts.ex
+++ apps/yellow_dog_console/lib/yellow_dog/console/components/layouts.ex
@@ -413,7 +413,7 @@ defmodule YellowDog.Console.Layouts do
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01"
+                    d="M13 10V3L4 14h7v7l9-11h-7z"
                   />
                 </svg>
                 <span>Netboot</span>
