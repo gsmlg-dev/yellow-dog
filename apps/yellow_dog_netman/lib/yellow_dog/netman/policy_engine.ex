012e4d56a46a58ed433753c466e6a02fe0e82048
Jonathan Gao <gsmlg.com@gmail.com>
2026-03-02 17:39:40 +0800

    refactor(netman): remove dead code in PolicyEngine.default_route/1
    Remove unreachable `[] -> :none` clause after Enum.sort_by on a
    non-empty list. The empty list case is already handled by the function
    head `def default_route([]), do: :none`. PolicyEngine coverage now 100%.

--- apps/yellow_dog_netman/lib/yellow_dog/netman/policy_engine.ex
+++ apps/yellow_dog_netman/lib/yellow_dog/netman/policy_engine.ex
@@ -30,10 +30,10 @@ defmodule YellowDog.Netman.PolicyEngine do
   def default_route([]), do: :none
 
   def default_route(active_connections) do
-    case Enum.sort_by(active_connections, &{-effective_priority(&1), &1[:profile_id] || &1[:id]}) do
-      [best | _] -> {:ok, best[:profile_id] || best[:id]}
-      [] -> :none
-    end
+    [best | _] =
+      Enum.sort_by(active_connections, &{-effective_priority(&1), &1[:profile_id] || &1[:id]})
+
+    {:ok, best[:profile_id] || best[:id]}
   end
 
   @doc """
