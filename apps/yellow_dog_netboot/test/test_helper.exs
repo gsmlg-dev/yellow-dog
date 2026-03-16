72e4417a63d1efd83e431f0e0d76e00fcd78da84
Jonathan Gao <gsmlg.com@gmail.com>
2026-03-17 02:53:23 +0800

    fix(test): start supervision trees in test_helpers for removed apps
    Netboot and DhcpClient tests relied on auto-started Application modules.
    Start their supervision trees explicitly in test_helper.exs.

--- apps/yellow_dog_netboot/test/test_helper.exs
+++ apps/yellow_dog_netboot/test/test_helper.exs
@@ -1,1 +1,5 @@
+# Start the Netboot supervision tree since the Application module was removed.
+# Tests depend on named processes (Asset.Store, ScriptEngine, TransferSupervisor, etc.)
+{:ok, _} = YellowDog.Netboot.Supervisor.start_link([])
+
 ExUnit.start()
