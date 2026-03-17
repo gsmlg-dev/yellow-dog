f240b93dee43f8645aa6c29af40c31c5cb195cab
Jonathan Gao <gsmlg.com@gmail.com>
2026-03-17 18:14:02 +0800

    fix(test): start DhcpClient supervision tree in netman test_helper
    The netman FSM tests call DhcpClient.start_interface/2 which requires
    the DhcpClient.Registry and DynamicSupervisor. Since the DhcpClient
    Application module was converted to a Supervisor, it must be explicitly
    started in dependent apps' test helpers.

--- apps/yellow_dog_netman/test/test_helper.exs
+++ apps/yellow_dog_netman/test/test_helper.exs
@@ -2,4 +2,9 @@ Application.put_env(:yellow_dog_netman, :profile_dir, Path.join(__DIR__, "suppor
 Application.put_env(:yellow_dog_netman, :reconciliation_interval_ms, 60_000)
 Application.put_env(:yellow_dog_netman, :netlink_backend, :mock)
 
+# Start the DHCP client supervision tree since it was converted from an
+# Application to a Supervisor. Netman FSM tests call DhcpClient.start_interface/2
+# which requires Registry and DynamicSupervisor to be running.
+{:ok, _} = YellowDog.DhcpClient.Application.start_link([])
+
 ExUnit.start(exclude: [:integration, :privileged])
