cb90bb7691c0d674b4a880211bfe12c64e477a29
Jonathan Gao <gsmlg.com@gmail.com>
2026-06-29 23:43:49 +0800

    fix(tasks): remove unsupported region sync task

--- apps/yellow_dog_tasks/lib/yellow_dog/tasks/region_data/provider.ex
+++
@@ -1,7 +0,0 @@
-defmodule YellowDog.Tasks.RegionData.Provider do
-  @moduledoc """
-  Source behavior for region data synchronization.
-  """
-
-  @callback list_countries() :: {:ok, [map()]} | {:error, term()}
-end
